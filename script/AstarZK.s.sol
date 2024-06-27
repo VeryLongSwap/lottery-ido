4a30f2e59399d03bf7d2725f2d23ddf86ec886ea
Hayatti <gurujowa@gmail.com>
2024-06-27 10:53:31 +0000

    USDC set decimals 6

--- script/AstarZK.s.sol
+++ script/AstarZK.s.sol
@@ -2,15 +2,15 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
-import { OverflowICO } from "../src/lottery.sol";
-import { ERC20 } from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
-import { IERC20 } from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import {OverflowICO} from "../src/lottery.sol";
+import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
 contract IDOStart is Script {
     address public USDC = 0xA8CE8aee21bC2A48a5EF670afCc9274C7bbbC035;
     address public BONSAI = 0xcE3F71Af1A80390cAb3f429B30567cBE0E853f4d;
-    OverflowICO public ido = OverflowICO(0xebA55284dA75BbAd9e00d112D468Ee7e3aab3799);
-
+    OverflowICO public ido =
+        OverflowICO(0x440b4eE171f175cA0eF03bd37E3fbeD46Ae09FC6);
 
     function run() public {
         vm.startBroadcast();
@@ -26,28 +26,25 @@ contract IDOStart is Script {
 contract Deploy is Script {
     OverflowICO public ido;
     ERC20 public buyerToken;
-    
+
     uint public startTime = 1719608400;
     uint public endTime = 1719651600;
     uint public tokensToSell = 1e18;
     address public dead = 0x000000000000000000000000000000000000dEaD;
-    
+
     address public USDC = 0xA8CE8aee21bC2A48a5EF670afCc9274C7bbbC035;
     address deployer = 0x944C6C8882012CcD4FFd2911a7F1fDC520c9a561;
     address public BONSAI = 0xcE3F71Af1A80390cAb3f429B30567cBE0E853f4d;
-    
 
     IERC20[] public buyerTokens;
-    uint[] public tokensPerTickets = [10 * 1e18];
+    uint[] public tokensPerTickets = [10 * 1e6];
 
     function run() public {
         vm.startBroadcast();
         buyerTokens.push(IERC20(USDC));
 
-
         // Ensure USDC contract exists and has code
         require(address(USDC).code.length > 0, "USDC contract does not exist");
-
 
         ido = new OverflowICO(
             IERC20(address(BONSAI)),
@@ -66,5 +63,4 @@ contract Deploy is Script {
 
         vm.stopBroadcast();
     }
-
 }
